196576e1a82cbd56d9ae55bd57d8d86210071c3b
patymori <excermori@gmail.com>
2016-10-27 10:48:25 -0200

    Corrected Post's create_date field definition and post_list order_by condition

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    create_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(null=True, blank=True)
 
     def publish(self):
