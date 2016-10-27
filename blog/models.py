2b417ae54cd81d4776bbabd408712c4f48f8a1ea
patymori <excermori@gmail.com>
2016-10-27 10:14:47 -0200

    Corrected Post and updated post_list template and view

--- blog/models.py
+++ blog/models.py
@@ -5,11 +5,11 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(null=True, blank=True)
 
     def publish(self):
-        self.published_date = timezone.now
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
