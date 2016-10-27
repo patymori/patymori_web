f4b2c20a48a47dd8c7efb8c6bcb4c3e14efb9916
patymori <excermori@gmail.com>
2016-10-27 15:51:34 -0200

    Changed template structure and CSS

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from . import views
 
+app_name = 'blog'
 urlpatterns = [
-    url(r'^$', views.post_list)
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<post_id>\d+)/$', views.post_detail, name='post_detail'),
 ]
